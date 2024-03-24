8d394cf084280733621194c3bf20a9f8f4298bed
karim kamil <karimmahrouskamil@hotmail.com>
2024-03-24 09:32:13 +0200

    quick initialize for components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TaskListComponent } from './task/task-list/task-list.component';
+import { TaskDetailComponent } from './task/task-detail/task-detail.component';
+import { TaskModule } from './task/task.module';
 
-const routes: Routes = [];
+const routes: Routes = [ { path: 'tasks', component: TaskListComponent },
+{ path: 'tasks/:taskId', component: TaskDetailComponent }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes),TaskModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/task/task.module.ts
+++ src/app/task/task.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
-import { TaskDetailComponent } from './task-detail/task-detail.component';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { TaskListComponent } from './task-list/task-list.component';
+import { TaskDetailComponent } from './task-detail/task-detail.component';
 @NgModule({
   declarations: [
     TaskListComponent,
     TaskDetailComponent
-  ]
+  ],
+  imports: [
+    CommonModule,
+    HttpClientModule,
+    FormsModule
+  ],
 })
 export class TaskModule {
 }
